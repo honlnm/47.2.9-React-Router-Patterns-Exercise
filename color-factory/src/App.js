2d574fad6dedbff0eb4c8b1a9a3c747a34051901
Nicole Honl <nicole.m.honl@gmail.com>
2024-06-06 17:44:46 -0500

    finished part 2

--- color-factory/src/App.js
+++ color-factory/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Link } from "react-router-dom";
+import RouteList from "./RouteList";
 
 function App() {
+  const [colors, setColors] = useState([]);
+
+  const addColor = (color) => {
+    setColors(prevColors => [color, ...prevColors]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Welcome to the Color Factory!</h1>
+        <Link to="/colors/new">Add New Color</Link>
+        <RouteList colors={colors} addColor={addColor} />
+      </div>
+    </BrowserRouter>
   );
 }
 
